f4cdfda8eaa314ad2e00ac2bd7730945fdca5033
Einarr07 <aidenperse@gmail.com>
2025-11-17 11:47:08 -0500

    Concepts basics

---
+++ strings.js
@@ -0,0 +1,17 @@
+let string1 = 'Hello'
+let string2 = "JavaScript is cool"
+let string3 = `${string1} Jenny`
+let string4 = string1 + ' ' + string2
+
+
+console.log("String 1: " + string1)
+console.log("String 2: " + string2)
+console.log("String 3: " + string3)
+console.log("String 4: " + string4)
+
+let phrase = "This is a big phrase"
+console.log("String 5: " + phrase)
+console.log("Size of phrase: " + phrase.length)
+console.log("Lower : " + phrase.toLowerCase())
+console.log("Upper : " + phrase.toUpperCase())
+console.log("One word : " + phrase.substring(0, 4))
---
+++ variables.js
@@ -0,0 +1,4 @@
+let cajaDeAndy = 'Woody'
+const pi = 3.141516
+console.log(cajaDeAndy)
+console.log(pi)
