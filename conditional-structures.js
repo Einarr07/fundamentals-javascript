53a7c5bad7ddff539809fac81907ac9bd2ad5bc0
Einarr07 <aidenperse@gmail.com>
2025-11-17 12:35:39 -0500

    conditional structures

---
+++ conditional-structures.js
@@ -0,0 +1,9 @@
+let nombre = 'Martin'
+
+if (nombre === "Camilo") {
+    console.log(`Hi: ${nombre}`);
+} else if (nombre !== "Camilo") {
+    console.log(`I'm not Camilo, I'm ${nombre}`);
+} else {
+    console.log(`Not is String`)
+}
