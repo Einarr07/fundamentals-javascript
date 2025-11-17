441d1485f956d4ef9245f9a7e1405075c8e75f20
Einarr07 <aidenperse@gmail.com>
2025-11-17 12:01:53 -0500

    Operators

---
+++ operators.js
@@ -0,0 +1,12 @@
+// Comparison operators
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+console.log("Double equals (==): " + (a == b));
+console.log("Strict equals (===): " + (a === c));
+console.log("Not equal (!=): " + (a != b));
+console.log("Strict not equal (!==): " + (a !== c));
+console.log("Greater than (>): " + (a > b));
+console.log("Less or equal (<=): " + (a <= b));
