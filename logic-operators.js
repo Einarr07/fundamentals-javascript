1cc7e173581a9d062a7672b79dc77660878b17e7
Einarr07 <aidenperse@gmail.com>
2025-11-17 12:28:13 -0500

    Logic operators

---
+++ logic-operators.js
@@ -0,0 +1,7 @@
+const a = 10;
+const b = 20;
+const c = "10";
+
+console.log("And: " + ((a == b) && (a === c)))
+console.log("Or: " + ((a == b) || (a === c)));
+console.log("Different: " + !(a === b));
