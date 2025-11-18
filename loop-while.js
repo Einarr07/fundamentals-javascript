d8b277d475c6c7906285f15ef9d127fb6435c3b5
Einarr07 <aidenperse@gmail.com>
2025-11-18 08:40:39 -0500

    While

---
+++ loop-while.js
@@ -0,0 +1,16 @@
+let acount = 0
+while (acount <= 9) {
+    acount++;
+    console.log(`${acount}`);
+}
+
+
+// Do-While
+let number = 0
+console.log("*".repeat(10))
+console.log("I'm do-while")
+console.log("*".repeat(10))
+do {
+    console.log(`${number}`);
+    number++;
+} while (number <= 9)
